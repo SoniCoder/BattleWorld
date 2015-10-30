23266c15605ddb4ff0edd72384b9e4a3f2e7c4d5
root <root@localhost.localdomain>
2015-10-30 11:12:56 -0400

    Kali: Added Linux Support

--- FP/Engine.h
+++ FP/Engine.h
@@ -1,9 +1,15 @@
 #ifndef FP_ENGINE
 #define FP_ENGINE
 #include <iostream>
+/*
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+*/
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
 #include "fp_structs.h"
 void FP_Init(const char *title,
 	int x, int y, int w,
